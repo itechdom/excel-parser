2e7cbddbcda408bdc60cc7b3e082e8317d597e15
Sam Alghanmi <oalghnmi@gmail.com>
2019-06-24 13:24:05 -0400

    updated data

--- react+react-native/csv-service/scripts/csv-service.js
+++ react+react-native/csv-service/scripts/csv-service.js
@@ -1,6 +1,6 @@
 const fs = require("fs");
 const parse = require("csv-parse");
-let data = fs.readFileSync("./data.csv");
+let data = fs.readFileSync("./data-2.csv");
 
 const output = [];
 let row = 0;
@@ -16,6 +16,7 @@ parse(data, {
     while ((record = this.read())) {
       //first row
       if (row === 0) {
+        console.log(record, "record");
         record.map((r, i) => (obj[r] = []));
       }
       if (row > 0) {
@@ -29,5 +30,8 @@ parse(data, {
     }
   })
   .on("end", function() {
-    fs.writeFileSync("./medications.js", `export const data = ${JSON.stringify(obj)} ;`);
+    fs.writeFileSync(
+      "./medications.js",
+      `export const data = ${JSON.stringify(obj)} ;`
+    );
   });
